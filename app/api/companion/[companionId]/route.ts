74bc887c5fd62b1a80458bab0b30155658d10c4a
novicode <novicode54@gmail.com>
2023-08-12 00:10:10 +0200

    Chat Header + Delete

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,5 +1,6 @@
 import prismadb from "@/lib/prismadb";
 import { currentUser } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
@@ -24,8 +25,6 @@ export async function PATCH(
       return new NextResponse("Missing required fields", { status: 400 });
     }
 
-    console.log(params.companionId);
-
     const companion = await prismadb.companion.update({
       where: {
         id: params.companionId,
@@ -45,6 +44,31 @@ export async function PATCH(
     return NextResponse.json(companion);
   } catch (error) {
     console.error("COMPANION_PATCH", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { companionId: string } },
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const companion = await prismadb.companion.delete({
+      where: {
+        userId,
+        id: params.companionId,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.error("COMPANION_DELETE", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
---
+++ components/bot-avatar.tsx
@@ -0,0 +1,14 @@
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { Avatar } from "./ui/avatar";
+
+interface BotAvatarProps {
+  src: string;
+}
+
+export const BotAvatar = ({ src }: BotAvatarProps) => {
+  return (
+    <Avatar className="w-12 h-12">
+      <AvatarImage src={src} />
+    </Avatar>
+  );
+};
