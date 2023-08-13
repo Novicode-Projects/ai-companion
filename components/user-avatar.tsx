cc66eaa94a21321d6b73ba6c38d9ae18624ef6ba
novicode <novicode54@gmail.com>
2023-08-13 19:24:39 +0200

    Chat + Start Stripe

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -6,6 +6,7 @@ import { useUser } from "@clerk/nextjs";
 
 export const UserAvatar = () => {
   const { user } = useUser();
+
   return (
     <Avatar className="w-12 h-12">
       <AvatarImage src={user?.imageUrl} />
